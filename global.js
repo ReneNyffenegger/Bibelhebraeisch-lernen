8949b00792afffc5adb130cda7a5995c6812179d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-06-02 15:34:17 +0200

    + Buchstaben

--- global.js
+++ global.js
@@ -1,3 +1,27 @@
 
 var font_hebr = 'Times New Roman';
 var font_text = 'Liberation sans';
+
+var gStyleSheet;
+
+function insertStyleSheet() {
+
+  var style = document.createElement('style');
+  style.type = 'text/css';
+
+  document.head.appendChild(style);
+
+}
+
+function insertGlobalStyleSheet() {
+
+  insertStyleSheet();
+
+  gStyleSheet = document.styleSheets[0];
+  gStyleSheet.insertRule(' * {font-family: ' + font_text + '}', 0);
+
+}
+
+function addCssRule(rule) {
+  gStyleSheet.insertRule(rule, 0);
+}
